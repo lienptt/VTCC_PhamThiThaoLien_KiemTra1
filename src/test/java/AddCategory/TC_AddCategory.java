7e95affbd01353537cb5518aae863d8c6881f065
lienptt <lienptt.test@gmail.com>
2024-03-27 00:42:57 +0700

    Update notes for steps

--- src/test/java/AddCategory/TC_AddCategory.java
+++ src/test/java/AddCategory/TC_AddCategory.java
@@ -17,62 +17,65 @@ public class TC_AddCategory extends BaseTest {
     public static void openBrowser() {
         createDriver();
         login();
-        WebElement productTab = driver.findElement(By.xpath("//span[normalize-space()='Products']"));
-        productTab.click();
-        WebElement categoryTab = driver.findElement(By.xpath("//span[normalize-space()='Category']"));
-        categoryTab.click();
+        //Redirect category tab
+        driver.findElement(By.xpath("//span[normalize-space()='Products']")).click();
+        driver.findElement(By.xpath("//span[normalize-space()='Category']")).click();
     }
 
-    @Test
+    @Test (priority = 0)
     public void createNewCategory() throws InterruptedException {
-        WebElement addNewCategoryButton = driver.findElement(By.xpath("//a[@class='btn btn-primary']"));
-        addNewCategoryButton.click();
+        //Click button Add New Category
+        driver.findElement(By.xpath("//a[@class='btn btn-primary']")).click();
+
         WebElement addNewCategoryForm = driver.findElement(By.cssSelector(".form-horizontal"));
         Boolean formIsDisplayed = addNewCategoryForm.isDisplayed();
         Assert.assertTrue(formIsDisplayed, "Assert Add new category form is displayed");
 
-        WebElement nameInput = driver.findElement(By.id("name"));
-        nameInput.sendKeys("T-shirt");
+       driver.findElement(By.id("name")).sendKeys("T-shirt");
 
+       // Choose Parent Category
         driver.findElement(By.cssSelector("button[title='No Parent']")).click();
         driver.findElement(By.xpath("(//input[@aria-label='Search'])[1]")).sendKeys("aka",Keys.ENTER);
         Actions action = new Actions(driver);
         action.sendKeys(Keys.ENTER).perform();
 
-        WebElement orderingNumber = driver.findElement(By.id("order_level"));
-        orderingNumber.sendKeys("2");
+        // Input Order level
+        driver.findElement(By.id("order_level")).sendKeys("2");
 
+        // Choose Type
         Select select = new Select(driver.findElement(By.xpath("//select[@name='digital']")));
         select.selectByVisibleText("Digital");
 
+        // Choose Filtering Attributes
         driver.findElement(By.cssSelector("button[title='Nothing selected']")).click();
         select = new Select(driver.findElement(By.xpath("//select[@name='filtering_attributes[]']")));
         select.selectByValue("3");
 
+        // Input meta title
         WebElement metaTileTextbox = driver.findElement(By.xpath("//input[@placeholder='Meta Title']"));
         metaTileTextbox.sendKeys("Test Add Category");
 
-        WebElement bannerChooseFile = driver.findElement(By.xpath("(//div[contains(text(),'Browse')])[1]"));
-        bannerChooseFile.click();
+        // Choose banner
+        driver.findElement(By.xpath("(//div[contains(text(),'Browse')])[1]")).click();
         List<WebElement> imageItems = driver.findElements(By.cssSelector(".aiz-uploader-select"));
         imageItems.get(1).click();
         WebElement buttonAddFile = driver.findElement(By.xpath("//button[normalize-space()='Add Files']"));
         buttonAddFile.click();
         Thread.sleep(10);
 
+        // Choose icon
         driver.findElement(By.xpath("(//div[contains(text(),'Browse')])[2]")).click();
         List<WebElement> imageItem = driver.findElements(By.cssSelector(".aiz-uploader-select"));
-        imageItem.get(1).click();
+        imageItem.get(2).click();
         driver.findElement(By.xpath("//button[normalize-space()='Add Files']")).click();
 
-        WebElement metaDes = driver.findElement(By.xpath("//textarea[contains(@name,'meta_description')]"));
-        metaDes.sendKeys("Category Description");
+        //Input meta description
+        driver.findElement(By.xpath("//textarea[contains(@name,'meta_description')]")).sendKeys("Category Description");
 
-        WebElement buttonSave = driver.findElement(By.cssSelector("button[type='submit']"));
-        buttonSave.click();
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
     }
 
-    @Test
+    @Test (priority = 1)
     public void assertSearchItem() throws InterruptedException {
         driver.findElement(By.xpath("//input[@id='search']")).sendKeys("T-shirt", Keys.ENTER);
         sleep(30);
